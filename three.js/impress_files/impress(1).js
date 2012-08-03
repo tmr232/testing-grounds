990764c6ea2330b691d9d9b189dc1d6459a8f3c6
unknown <tamir.login.email@gmail.com>
2012-08-03 16:29:52 +0300

    some more testing

---
+++ three.js/impress_files/impress(1).js
@@ -0,0 +1,46 @@
+callback({
+  "data": {
+    "watchers_count": 10599,
+    "ssh_url": "git@github.com:bartaz/impress.js.git",
+    "forks_count": 1587,
+    "has_issues": true,
+    "created_at": "2011-12-28T22:23:19Z",
+    "owner": {
+      "gravatar_id": "b1d34875b6175ed2b5377b13a4782dba",
+      "avatar_url": "https://secure.gravatar.com/avatar/b1d34875b6175ed2b5377b13a4782dba?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png",
+      "url": "https://api.github.com/users/bartaz",
+      "id": 83575,
+      "login": "bartaz"
+    },
+    "open_issues_count": 29,
+    "has_wiki": true,
+    "svn_url": "https://github.com/bartaz/impress.js",
+    "network_count": 1587,
+    "watchers": 10599,
+    "updated_at": "2012-08-03T07:35:45Z",
+    "full_name": "bartaz/impress.js",
+    "open_issues": 29,
+    "homepage": "http://bartaz.github.com/impress.js",
+    "clone_url": "https://github.com/bartaz/impress.js.git",
+    "size": 252,
+    "fork": false,
+    "pushed_at": "2012-05-05T11:59:32Z",
+    "master_branch": "master",
+    "forks": 1587,
+    "language": "JavaScript",
+    "name": "impress.js",
+    "url": "https://api.github.com/repos/bartaz/impress.js",
+    "mirror_url": null,
+    "has_downloads": true,
+    "description": "It's a presentation framework based on the power of CSS3 transforms and transitions in modern browsers and inspired by the idea behind prezi.com.",
+    "git_url": "git://github.com/bartaz/impress.js.git",
+    "private": false,
+    "id": 3065454,
+    "html_url": "https://github.com/bartaz/impress.js"
+  },
+  "meta": {
+    "status": 200,
+    "X-RateLimit-Limit": "5000",
+    "X-RateLimit-Remaining": "4999"
+  }
+})
